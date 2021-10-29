36fd2984cd34f88e574fbffc3adaf86b1fab8030
James Haggerty <jpm.haggerty@gmail.com>
2021-10-29 15:02:36 -0400

    Co-authored-by: jbuch1964 <jbuch1964@users.noreply.github.com> Co-authored-by: Dustin Stringer <neg-0@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -1,44 +1,40 @@
 // Update with your config settings.
 
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    client: "pg",
+    connection: "postgres://postgres:docker@localhost/pokemon"
   },
 
   staging: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
 
   production: {
-    client: 'postgresql',
+    client: "postgresql",
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      database: "my_db",
+      user: "username",
+      password: "password",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
+      tableName: "knex_migrations",
+    },
+  },
 };
