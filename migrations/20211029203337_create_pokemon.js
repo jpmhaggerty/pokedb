4ff2ec1436dd7e56325f72a75ff42d045b2cb901
James Haggerty <jpm.haggerty@gmail.com>
2021-11-01 12:34:18 -0400

    Mostly functional x2

--- migrations/20211029203337_create_pokemon.js
+++ migrations/20211029203337_create_pokemon.js
@@ -1,6 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('pokemon', table => {
         table.increments('id'); // adds an auto incrementing PK column
+        table.integer('pokemon_id');
         table.string('name').notNullable();
         table.integer('height');
         table.integer('weight');
